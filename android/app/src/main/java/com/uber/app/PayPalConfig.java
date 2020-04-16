c20947fc53a9fc6de83f6d1ef600db70b7d2f280
Dexter <49821562+dextersharma03@users.noreply.github.com>
2020-04-15 23:45:23 -0700

    Update PayPalConfig.java

--- android/app/src/main/java/com/uber/app/PayPalConfig.java
+++ android/app/src/main/java/com/uber/app/PayPalConfig.java
@@ -1,6 +1,6 @@
 package com.uber.app;
 
 public class PayPalConfig {
-    public static final String PAYPAL_CLIENT_ID = "Af-x1I7yP8492Qy9m7vrfCsL4ZZNx6h3J4vP885LnVVKVzJA18O73GxkbTX-UrhjKLP6BDLX-BHztEw_";
+    public static final String PAYPAL_CLIENT_ID = "ID";
 
 }
